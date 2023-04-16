16126c30ad6d35ac48f9a14d0b9bf94b3425c3ae
discphy <dinophy@nate.com>
2023-04-16 17:19:21 +0900

    Initial commit

---
+++ src/main/java/com/discphy/batch/demo/BatchDemoApplication.java
@@ -0,0 +1,15 @@
+package com.discphy.batch.demo;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class BatchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/discphy/batch/demo/BatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discphy.batch.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
